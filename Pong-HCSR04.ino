5a1db5fc61c075877ee03d78daa21dfe7fd936a0
Thomas <thomas@healfies.com>
2017-03-12 02:39:11 -0300

    add files

---
+++ Pong-HCSR04.ino
@@ -0,0 +1,33 @@
+/*
+ * HCSR04Ultrasonic/examples/UltrasonicDemo/UltrasonicDemo.pde
+ *
+ * SVN Keywords
+ * ----------------------------------
+ * $Author: cnobile $
+ * $Date: 2011-09-17 02:43:12 -0400 (Sat, 17 Sep 2011) $
+ * $Revision: 29 $
+ * ----------------------------------
+ */
+
+#include <Ultrasonic.h>
+
+#define TRIGGER_PIN  12
+#define ECHO_PIN     13
+
+Ultrasonic ultrasonic(TRIGGER_PIN, ECHO_PIN);
+
+void setup()
+  {
+  Serial.begin(9600);
+  }
+
+void loop()
+  {
+  float cmMsec;
+  long microsec = ultrasonic.timing();
+
+  cmMsec = ultrasonic.convert(microsec, Ultrasonic::CM);
+  cmMsec = cmMsec;
+  Serial.println(cmMsec);
+  delay(20);
+  }
